58d189ac686cf29b7c8a85872116833ee18ed7e0
Olivia Burke <oburke@quotemedia.com>
2022-08-09 01:06:02 -0700

    loop a few times until valid word

--- randet.py
+++ randet.py
@@ -8,6 +8,13 @@ app = flask.Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
 def randet():
-	blob = request.urlopen("https://random-word-api.herokuapp.com/word").read()
-	word = json.loads(blob)[0]
-	return("<h2>" + word + ":</h2><br>" + str(ety.origins(word, recursive=True)))
+	count = 0
+	while count < 10
+		blob = request.urlopen("https://random-word-api.herokuapp.com/word").read()
+		word = json.loads(blob)[0]
+		et = ety.origins(word, recursive=True)
+		if len(et) > 0:
+			break
+		else:
+			count += 1
+	return("<h2>" + word + ":</h2><br>" + str(et))
