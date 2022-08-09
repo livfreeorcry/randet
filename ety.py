2e171104a005745c41391d2cfd9df5c6c539a924
Olivia Burke <oburke@quotemedia.com>
2022-08-09 00:31:46 -0700

    random word lookup

--- ety.py
+++ ety.py
@@ -1,10 +1,14 @@
 import os
 import re
 from random import randint
+from json import loads
+from urllib import request
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
 def randet():
-	return("Hello")
+	blob = request.urlopen("https://random-word-api.herokuapp.com/word").read()
+	word = json.loads(blob)[0]
+	return(word)
