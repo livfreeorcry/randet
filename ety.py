12a04a058a6f0279ac788b07be9af63ac65839d2
Olivia Burke <oburke@quotemedia.com>
2022-08-09 00:01:11 -0700

    flask imports

---
+++ ety.py
@@ -0,0 +1,10 @@
+import os
+import re
+from random import randint
+from flask import Flask, render_template, requests, redirect, url_for, jsonify
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET','POST'])
+def randet:
+	return("Hello")
