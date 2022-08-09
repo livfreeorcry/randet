f76d925b7547009e14a0c61b2a3d962a475b6f8b
Olivia Burke <oburke@quotemedia.com>
2022-08-09 01:08:41 -0700

    message

--- randet.py
+++ randet.py
@@ -9,12 +9,13 @@ app = flask.Flask(__name__)
 @app.route('/', methods=['GET','POST'])
 def randet():
 	count = 0
-	while count < 10
+	while count < 10:
 		blob = request.urlopen("https://random-word-api.herokuapp.com/word").read()
 		word = json.loads(blob)[0]
 		et = ety.origins(word, recursive=True)
 		if len(et) > 0:
+			return("<h2>" + word + ":</h2><br>" + str(et))
 			break
 		else:
 			count += 1
-	return("<h2>" + word + ":</h2><br>" + str(et))
+	return("<h2>Tried ten words, none had an etymology to provide. Please try again.</h2")
